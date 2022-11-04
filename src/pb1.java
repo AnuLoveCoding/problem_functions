7da833034b75bda83866f2d97d61113da61d02c4
anuLoveCoading <anusrivas07@gmail.com>
2022-11-04 18:51:59 +0530

    git commit -m "function-problem"

--- src/pb1.java
+++ src/pb1.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class pb1 {
+
+
+
+
+ //    @Override
+    //find the factorial of n;
+    public  static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <=n ; i++) {
+            fact*=i;
+        }
+        return fact;
+    }
+
+    // Main methods ;
     public static void main(String[] args) {
-        System.out.println("welcome function probelm");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Your number ");
+        int n  = scn.nextInt();
+
+        int ans =  factorial(n);
+        System.out.println("The Factorial of n : "+ans);
+
+        /*
+         ***** check-file-location *****;
+        pb1 file = new pb1();
+        System.out.println(file);
+        */
     }
 }
