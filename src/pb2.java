5fe360a494d026a0524a2dc6563bde4d28ab74c0
anuLoveCoading <anusrivas07@gmail.com>
2022-11-05 09:57:43 +0530

    git commit -m "function-problem"

--- src/pb2.java
+++ src/pb2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class pb2 {
 //    Q .1 Given two numbers,Return their sum;
 //    public  static int sum(int a , int b) {
@@ -5,24 +7,57 @@ public class pb2 {
 //       return  ans ;
 //    }
 
-//    Q.2 Given two numbers A & B. Return their product.
-//    public static int product(final int a, final int b, final int c) {
+//    Q.2 Given two numbers A & B & C. Return their product.
+//    public static int product(final int A, final int B, final int C) {
 //        return a*b*c;
 //    }
 
 //    Q.3 Given a number A. Return square of the number.
-//    public static int sqaure(int a ){
+//    public static int square(int a ){
 //        int ans = a*a;
 //        return ans;
 //    }
 
+//    Q.4 Given two integers A and B, where A is divisible by B.
+//      public static double divisible(final double A , final double B){
+//          return A/B;
+//      }
+
+//    Q.5 You have to find the value of cube of A i.e, A3.
+//     public static  int cube( final int n , final int m){
+//            int product =1;
+//         for (int i = 1; i <=m ; i++) product*=n;
+//         return product;
+//     }
+
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Your Number for 1 ");
+        int n = scn.nextInt();
+        System.out.println("Enter Your Number for 2 ");
+        int m = scn.nextInt();
 //        Q.1 returning value;
 //        System.out.println(sum(9,17));
+
 //        Q.2 returning value;
 //        System.out.println(product(4,5,6));
+
 //        Q.3 returning value;
-//        int ans = sqaure(5);
+//        int ans = square(5);
 //        System.out.println("This is Your Square Value "+"= "+ ans);
+
+//        Q.4 returning value;
+//        System.out.println(divisible(2,2));
+//        System.out.println(divisible(12,6));
+//        System.out.println(divisible(7,3));
+//        System.out.println(divisible(15,6));
+//        System.out.println(divisible(21,4));
+
+//        Q.5 returning Value;
+//        System.out.println("Cube of number is "+cube(n,m));
+//        System.out.println("Cube of number is "+cube(6,3));
+//        System.out.println("Cube of number is "+cube(5,2));
+//        System.out.println("Cube of number is "+cube(8,3));
+//        System.out.println("Cube of number is "+cube(16,3));
     }
 }
