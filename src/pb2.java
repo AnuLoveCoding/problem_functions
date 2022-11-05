6899b5f45c396c358572413cc45afd6e8743606a
anuLoveCoading <anusrivas07@gmail.com>
2022-11-05 09:08:46 +0530

    git commit -m "function-problem"

---
+++ src/pb2.java
@@ -0,0 +1,28 @@
+public class pb2 {
+//    Q .1 Given two numbers,Return their sum;
+//    public  static int sum(int a , int b) {
+//        int ans = a+b;
+//       return  ans ;
+//    }
+
+//    Q.2 Given two numbers A & B. Return their product.
+//    public static int product(final int a, final int b, final int c) {
+//        return a*b*c;
+//    }
+
+//    Q.3 Given a number A. Return square of the number.
+//    public static int sqaure(int a ){
+//        int ans = a*a;
+//        return ans;
+//    }
+
+    public static void main(String[] args) {
+//        Q.1 returning value;
+//        System.out.println(sum(9,17));
+//        Q.2 returning value;
+//        System.out.println(product(4,5,6));
+//        Q.3 returning value;
+//        int ans = sqaure(5);
+//        System.out.println("This is Your Square Value "+"= "+ ans);
+    }
+}
